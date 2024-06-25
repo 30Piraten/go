786095cdc1ea14e23922f788329a48650706aa37
30Piraten <kioyarautenberg@gmail.com>
2024-06-25 05:37:31 +0200

    Added comments to pointers/1/2

--- 2-pro-struct/pointers1.go
+++ 2-pro-struct/pointers1.go
@@ -9,14 +9,24 @@ import (
 var n = flag.Bool("n", false, "remove trailing newline")
 var sep = flag.String("s", " ", "separator")
 
+// pointers are a key to the flag package
+// this uses a program's cmd line args
+// to set values of variables in a program
 func main() {
 
-	// pointers are a key to the flag package
-	// this uses a program's cmd line args
-	// to set values of variables in a program
+	// flag.Parse() is called before the
+	// flags are used to update the flag
+	// variables from their default values
 	flag.Parse()
+
+	// non-flag args, i.e flag.Args() are
+	// available as a slice of strings
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
 		fmt.Println()
 	}
 }
+
+// go run pointers1.go some words are
+// go run pointers1.go -s & some words are
+// go run pointers1.go -n some words are
