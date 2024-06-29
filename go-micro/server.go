0a9c36b334fc73cb10c6a24a7ec3f17c68e28d4d
30Piraten <kioyarautenberg@gmail.com>
2024-06-29 04:53:30 +0200

    Updated go-micro/server.go with Marshal

--- go-micro/server.go
+++ go-micro/server.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
+
+type helloHandlerResponse struct {
+	Message    string `json:"message"`
+	Occupation string `json:"occupation"`
+	ID         int    `json:"id"`
+}
 
 func main() {
 	port := 2323
 
-	http.HandleFunc("/helloworld", helloWorldHandler)
+	http.HandleFunc("/helloworld", helloHandler)
 
-	log.Printf("Server starting on port: %v\n", 2323)
+	log.Printf("Server starting on port: %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Willkomen!\n")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	res := helloHandlerResponse{
+		Message:    "Willkommen",
+		Occupation: "Cloud Engineer",
+		ID:         203511,
+	}
+	data, err := json.Marshal(res)
+	if err != nil {
+		panic("Told you so!")
+	}
+	fmt.Fprint(w, string(data))
 }
