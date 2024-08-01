b4dc62b892ffce86eac1b8a41ce15a046e705d3c
30Piraten <victorehikioyaraeva@outlook.com>
2024-08-01 11:00:59 +0200

    Updated and added package-initialization

---
+++ 2-pro-struct/package-initialization/go.mod
@@ -0,0 +1,3 @@
+module github.com/30Piraten/go/package-initialization
+
+go 1.22.5
---
+++ 2-pro-struct/package-initialization/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/30Piraten/go/package-initialization/popcount"
+)
+
+func main() {
+	fmt.Println(popcount.PopCount(255))
+}
